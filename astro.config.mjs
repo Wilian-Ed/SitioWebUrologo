d2141a0b4cd8c77a1baeb79386346c87addbdb08
Wilian Soch <55908367+Wilian-Ed@users.noreply.github.com>
2026-02-12 20:06:32 -0600

    agregando sitio

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sistemasdeguatemala.com.gt',
+  base: '/Dr%20Guillermo%20Ixquiac',
+});
